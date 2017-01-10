f363c07f9eba7ec573978b9269816c3ff0fad284
juanchaves <chavesjuanm@gmail.com>
2017-01-09 22:18:00 -0800

    Adds:
    added icons to file, and project/projectArticles js file. made chenges to HTML

---
+++ scripts/projectArticles.js
@@ -0,0 +1,26 @@
+var projectArticle = [
+  {
+    title:       'About Me',
+    author:      'Juan Chaves',
+    contributors: 'Annie Ton-Nu',
+    projectUrl:   'https://juanchaves.github.io/about_me/',
+    mainlanguage: 'JavaScript',
+    body:         '<p>Description Here</p>'
+  },
+  {
+    title:        'Salmon Cookies',
+    author:       'Juan Chaves',
+    contributors: 'None',
+    projectUrl:   'https://juanchaves.github.io/salmon_cookies/',
+    mainlanguage: 'CSS',
+    body:         '<p>Description Here</p>'
+  },
+  {
+    title:        'Wire Frame Exercise',
+    author:       'Juan Chaves',
+    contributors: 'None',
+    projectUrl:   'https://juanchaves.github.io/wirefame_exercise/',
+    mainlanguage: 'CSS',
+    body:         '<p>Description Here</p>'
+  }
+];
