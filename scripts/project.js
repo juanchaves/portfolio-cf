3939d9ecbf052bee99157f9ceab092eaed5b3dd1
juanchaves <chavesjuanm@gmail.com>
2017-01-12 16:44:36 -0800

    Linked JS files to index and built constructor:
    started a buch of stuff from scratch

--- scripts/project.js
+++ scripts/project.js
@@ -1,43 +1,7 @@
-var articles = [];
+var Projects = [];
 
-function Project (options) {
-/* TODO: This is our Model constructor. It will take in
-   our source data from articleProjects and instantiate a
-   new Object according to this new definition. options is
-   a placeholder for the object that will ultimately be
-   passed in. Use all of the properties in blogProjects
-   to populate the new Project data that we'll use.  */
-   this.title = options.title;
-};
-
-Project.prototype.toHtml = function() {
-  var $newProject = $('article.template').clone();
-  $newArticle.attr('data-category', this.category);
-  /* TODO: We also need to fill in:
-  1. author name
-  2. author url
-  3. article title
-  4. article body
-  5. publication*/
-/* TODO: This cloned article is no longer a template, as it now
-has real data attached to it. Remove the class from this new article! */
-  return $newProject;
-};
-
-/* This sort method is a standard JavaScript Array function
-   that will iterate over an array and compare its values,
-   and then arrange them in ascending or descending order
-   according to the return value. We are comparing the
-   publishedOn properties to arrange the blog posts in
-   descending order (most recent first). */
-projectArticle.sort(function(currentObject, nextObject) {
-  return (new Date(nextObject.publishedOn)) - (new Date(currentObject.publishedOn));
-});
-
-projectArticle.forEach(function(ele) {
-  articles.push(new Article(ele));
-});
-
-articles.forEach(function(article) {
-  $('#articles').append(article.toHtml());
-});
+function Project (obj) {
+  for (var key in obj){
+    this[key] = obj[key]
+  }
+}
